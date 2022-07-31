71c870abfacebc2ff02c8ad519a8a72a41818167
Sebastian <sebastian.presser@gmail.com>
2022-07-31 14:52:51 +0200

    Limited amount of drawn stack cards

--- uno/components/cardstack.py
+++ uno/components/cardstack.py
@@ -13,11 +13,17 @@ class CardStack:
         self.card_back_img = img
         self.pos = pos
         self.size = size
-        self.v_dist = 2
-        self.stack_height = [230, 460, 680, 900, 1120, 1340, 1560, 1780, 2000]
+        self.v_dist = 5
+        #self.stack_height = [230, 460, 680, 900, 1120, 1340, 1560, 1780, 2000]
+        self.stack_height = []
+        for i in range(1, 10):
+            self.stack_height.append(i*60)
         self.reset()
 
     def reset(self) -> None:
+        """
+        Resets the surface and stack size to its default value
+        """
         self.stack_img = Surface(self.size)
         self.stack_size = 0
     
@@ -29,11 +35,11 @@ class CardStack:
             self.g.blit_aligned(self.stack_img, self.pos)
     
     def add_cards(self, num : int = 1) -> None:
+        """
+        Adds cards to the card stack
+        :param num: optionally, specify the number of cards to add to the stack
+        """
         for i in range(0, num):
-            #rowid = (self.stack_size**1.3)//500
-            #x_pos = (rowid * self.size[0]) * 1.5
-            #y_pos = self.size[1] - 50 -(rowid*60) - self.stack_size*self.v_dist
-
             if self.stack_size < self.stack_height[0]:
                 pos = (self.size[0]//2, self.size[1] - 100 - self.stack_size*self.v_dist)
             elif self.stack_size < self.stack_height[1]:
@@ -44,9 +50,10 @@ class CardStack:
                 pos = (self.size[0]//2 - 80, self.size[1] - 35 - (self.stack_size-self.stack_height[2])*self.v_dist)
             elif self.stack_size < self.stack_height[4]:
                 pos = (self.size[0]//2, self.size[1] - 35 - (self.stack_size-self.stack_height[3])*self.v_dist)
-            else: #if self.stack_size < self.stack_height[5]:
+            elif self.stack_size < self.stack_height[5]:
                 pos = (self.size[0]//2 + 80, self.size[1] - 35 - (self.stack_size-self.stack_height[4])*self.v_dist)
-            
+            else:
+                return
             self.g.blit_aligned(self.card_back_img, pos, self.stack_img)
             self.stack_size += 1
         
